341bb9c84f6d2cdcfbc23caf305fd73f225ea5e8
Ken Rebane <rebaneken95@gmail.com>
2018-02-12 07:22:35 +0200

    Created a datastore module

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,5 +1,30 @@
+//IIFE for namespaces
 (function(window){
   'use strict';
 
-  //Code
+  var App = window.App || {};
+
+  //Creating a Data store constructor for storing order info
+  function DataStore() {
+    this.data = {};
+  }
+
+  DataStore.prototype.add = function(key, val) {
+    this.data[key] = val;
+  }
+
+  DataStore.prototype.get = function(key) {
+    return this.data[key];
+  }
+
+  DataStore.prototype.getAll = function() {
+    return this.data;
+  }
+
+  DataStore.prototype.remove = function(key) {
+    delete this.data[key];
+  }
+
+  App.DataStore = DataStore;
+  window.App = App;
 })(window);
