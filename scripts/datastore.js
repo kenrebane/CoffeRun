36e952b4a79c54557537e520400124ac8b43597d
Ken Rebane <rebaneken95@gmail.com>
2018-02-12 09:59:54 +0200

    Main module for initializing code on page run

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,33 +1,44 @@
-//IIFE for namespaces
 (function(window){
   'use strict';
 
   var App = window.App || {};
+  var hidden = 0;
 
   /*
-  
-    Data store constructor for storing order info
-    create a new object var asd = new App.DataStore();
-    add data to it asd.add("asd@asd.com", "espresso");
-    same with get, getAll and remove;
-
+    Data store constructor for storing order info as objects
   */
   function DataStore() {
     this.data = {};
   }
 
+  /*
+    .add method takes key(email) and value(coffe type):
+     -assigns value to key inside constructors object
+  */
   DataStore.prototype.add = function(key, val) {
+    hidden++;
     this.data[key] = val;
   }
 
+  /*
+    .get method takes key(email) and
+    returns corresponding value(coffe type)
+  */
   DataStore.prototype.get = function(key) {
     return this.data[key];
   }
 
+  /*
+    .getAll method returns the whole DataStores object
+  */
   DataStore.prototype.getAll = function() {
     return this.data;
   }
 
+  /*
+    .remove method takes key(email) and
+    deletes the corresponding instance from DataStore object
+  */
   DataStore.prototype.remove = function(key) {
     delete this.data[key];
   }
---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+/*
+Receives the window object for function body.
+Retrieves constructors DataStore(datastore.js) and
+Truck(truck.js).
+*/
+(function(window) {
+  'use strict';
+
+  var App = window.App;
+
+  //Assigning constructors to local variables
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+
+  //Creating an instance of Truck
+  var myTruck = new Truck( "Super Coffe Truck", new DataStore() );
+
+
+  //Exporting the instance of Truck to global namespace
+  window.myTruck = myTruck;
+  
+})(window);
